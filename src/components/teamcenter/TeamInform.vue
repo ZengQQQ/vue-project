ccbc985910bcc2f99d1bd09d048de0cebf9b8252
Zcqer <2200304515@qq.com>
2024-05-08 15:53:33 +0800

    5月8日信息

--- src/components/teamcenter/TeamInform.vue
+++ src/components/teamcenter/TeamInform.vue
@@ -2,12 +2,19 @@
     <div class="team-notification-list">
       <h2>组队管理通知</h2>
       <el-tabs v-model="activeTab">
-        <el-tab-pane label="我发送的" name="sent">
+        <el-tab-pane label="队长消息" name="sent">
           <template v-for="notification in sentNotifications" :key="notification.tsm_id">
             <el-card>
               <p>队伍名称：{{ notification.teamFix.t_name }}</p>
               <p>申请人信息：{{ userInfoStore.info.u_name }}</p>
-              <p>接受者：{{ notification.user.u_name }} </p>
+
+            <p v-if="notification.teamFix.captain.u_acc === userInfoStore.info.u_acc">
+              接受者：{{ notification.user.u_name }} 
+              </p>
+            <p v-else>
+              接受者：{{ notification.teamFix.captain.u_name }}
+            </p>
+
               <p>申请时间：{{ notification.tsm_time }}</p>
               <p v-if="notification.tsm_pass === '0'">
                 状态：待处理
@@ -26,7 +33,7 @@
         </el-tab-pane>
         
 
-        <el-tab-pane label="我接收的" name="received">
+        <el-tab-pane label="队员消息" name="received">
           <template v-for="notification in receivedNotifications" :key="notification.tsm_id">
             <el-card>
               <p>申请队伍信息：{{ notification.teamFix.t_name }}</p>
@@ -60,37 +67,52 @@
   const activeTab = ref('sent');
   
 
-
   const loadNotifications = async () => {
-    // 调用后端接口获取组队管理通知数据
-
-    //作为队长发送的邀请，和接受的申请
-     
+  try {
     const capData = await fetchTeamCaptainApplication({
       u_acc: userInfoStore.info.u_acc,
     });
-    const CaptainNotifications=capData.data.listPage;
-
-    //作为成员发送的申请，和接受的邀请
     const memData = await fetchTeamMemberApplication({
       u_acc: userInfoStore.info.u_acc,
-    
     });
+    console.log("capData",capData);
+    console.log("memData",memData);
 
-    const MemberNotifications = memData.data.listPage;
+    if (capData.code === 200 && memData.code === 200) {
+      const CaptainNotifications = capData.data.listPage || [];
+      const MemberNotifications = memData.data.listPage || [];
 
+      const CaptainSentNotifications = CaptainNotifications.filter(
+        (notification) => notification.tsm_dct === '0'
+      );
+      const CaptainReceivedNotifications = CaptainNotifications.filter(
+        (notification) => notification.tsm_dct === '0'
+      );
 
-    const CaptainSentNotifications = CaptainNotifications.value.filter(notification => notification.user === '1');
-    const CaptainReceivedNotifications = CaptainNotifications.value.filter(notification => notification.tsm_dct === '0');
+      const MemberSentNotifications = MemberNotifications.filter(
+        (notification) => notification.tsm_dct === '0'
+      );
+      const MemberReceivedNotifications = MemberNotifications.filter(
+        (notification) => notification.tsm_dct === '0'
+      );
 
-    const MemberSentNotifications = MemberNotifications.value.filter(notification => notification.user === '1');
-    const MemberReceivedNotifications = MemberNotifications.value.filter(notification => notification.tsm_dct === '0');
-
-
-    sentNotifications.value = CaptainSentNotifications.concat(MemberSentNotifications);
-    receivedNotifications.value = CaptainReceivedNotifications.concat(MemberReceivedNotifications);
-  };
   
+      sentNotifications.value = CaptainSentNotifications.concat(MemberSentNotifications);
+      receivedNotifications.value = CaptainReceivedNotifications.concat(
+        MemberReceivedNotifications
+      );
+
+      console.log("sentNotifications",sentNotifications.value);
+      console.log("receivedNotifications",receivedNotifications.value);
+    } else {
+      // 处理数据获取失败的情况
+      console.error('Failed to fetch team notifications:', capData.message, memData.message);
+    }
+  } catch (error) {
+    console.error('Error fetching team notifications:', error);
+  }
+};
+
   const handleApprove = async (notification) => {
     // 处理同意申请逻辑
     await agreeJoinTeam(notification.id);
